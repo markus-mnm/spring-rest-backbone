840c63abea9c0c71a699ea1e8fa0e35054bf7a77
Markus Ebenhoeh <markus@ebenhoeh.at>
2014-07-01 16:18:35 +1000

    cleaning up for blog

--- Unfamous-Quotes/src/main/java/at/mnm/demo/unfamousquotes/DataSourceConfiguration.java
+++ Unfamous-Quotes/src/main/java/at/mnm/demo/unfamousquotes/DataSourceConfiguration.java
@@ -1,43 +1,30 @@
 package at.mnm.demo.unfamousquotes;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 public class DataSourceConfiguration {
    private static Log logger = LogFactory.getLog(DataSourceConfiguration.class);
 
    @Configuration()
-   // @Profile("default")
-   // public static class LocalConfiguration {
-   // @Bean(destroyMethod = "close")
-   // public DataSource dataSource() {
-   // logger.info(" local ");
-   // DataSource ds = new DataSource();
-   // ds.setDriverClassName(Driver.class.getName());
-   // ds.setUrl("jdbc:h2:mem");
-   // ds.setUsername("sa");
-   // ds.setPassword("");
-   // return ds;
-   // }
-   // }
-   //
-   // @Configuration()
-   // @Profile("cloud")
+   @Profile("local")
+   public static class LocalConfiguration {
+
+      public LocalConfiguration() {
+         logger.info(" LocalConfiguration: no data source set-up required because auto jpa - magic");
+      }
+   }
+
+   @Configuration()
+   @Profile({ "default" })
    public static class CloudConfiguration extends AbstractCloudConfig {
       @Bean(destroyMethod = "close")
-      public DataSource dataSource() {
-
-         logger.info(" cloud : ${cloud.services.spring-mysql.connection.username}");
-         // DataSource ds = new DataSource();
-         // ds.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
-         // ds.setUrl("jdbc:mysql://${cloud.services.spring-mysql.connection.host}:${cloud.services.spring-mysql.connection.port}/${cloud.services.spring-mysql.connection.name}");
-         // ds.setUsername("${cloud.services.spring-mysql.connection.username}");
-         // ds.setPassword("${cloud.services.spring-mysql.connection.password}");
+      public javax.sql.DataSource dataSource() {
+         logger.info(" CloudConfiguration : using connectionFactory to set data source");
          return connectionFactory().dataSource();
       }
    }
